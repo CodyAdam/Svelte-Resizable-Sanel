cc2f149069bf56f698d9768c30c4a0b5f964ab66
CodyAdam <cody.adam@laposte.net>
2020-06-16 11:42:41 +0200

    done

--- src/App.svelte
+++ src/App.svelte
@@ -2,7 +2,22 @@
   import ResizeBar from "./ResizeBar.svelte";
 </script>
 
-<ResizeBar direction="horizontal">
-  <div class="div1" slot="first">div1</div>
-  <div class="div2" slot="second">div2</div>
+<ResizeBar isVertical={true} pos={66}>
+  <div slot="1">
+    <ResizeBar isVertical={false}>
+      <div class="div1" slot="1">div1</div>
+      <div class="div2" slot="2">
+        <ResizeBar isVertical={true} pos={66}>
+          <div slot="1">
+            <ResizeBar isVertical={true}>
+              <div class="div2" slot="1">div3</div>
+              <div class="div2" slot="2">div4</div>
+            </ResizeBar>
+          </div>
+          <div class="div2" slot="2">div5</div>
+        </ResizeBar>
+      </div>
+    </ResizeBar>
+  </div>
+  <div class="div3" slot="2">div2</div>
 </ResizeBar>
--- src/ResizeBar.svelte
+++ src/ResizeBar.svelte
@@ -1,26 +1,39 @@
-<script>
-  import { onMount } from "svelte";
+<script lang="typescript">
+  export let isVertical: boolean = true;
+  export let max: number = 90;
+  export let min: number = 10;
+  export let thickness: number = 5;
+  export let pos: number = 50;
 
-  export let direction = "horizontal";
-
-  let dragging = false;
-  let size;
-  let startPos;
-  let pos = 50;
+  let div: HTMLElement;
+  let dragging: boolean = false;
+  let size: number;
+  let startPos: number;
 
   function handleMousedown(e) {
     dragging = true;
-    startPos = e.clientY;
+    updateSize();
+    startPos = isVertical ? e.clientX : e.clientY;
   }
+
   function handleMousemove(e) {
-    if (dragging) pos = (100 * e.clientY) / size;
+    if (dragging) {
+      console.log(size);
+      pos = (100 * (isVertical ? e.clientX : e.clientY)) / size;
+      pos = Math.min(Math.max(pos, min), max);
+    }
   }
+
   function handleMouseup(e) {
     dragging = false;
   }
+
+  function updateSize() {
+    size = isVertical ? div.clientWidth : div.clientHeight;
+  }
 </script>
 
-<style lang="scss">
+<style type="text/scss">
   .container {
     width: 100%;
     height: 100%;
@@ -29,28 +42,28 @@
   }
 
   .h-bar {
-    background-color: greenyellow;
-    height: 5px;
-    width: 100%;
+    background-color: #8bca67;
+    &:hover {
+      cursor: ns-resize;
+    }
   }
 
   .v-bar {
-    background-color: greenyellow;
-    height: 100%;
-    width: 5px;
+    background-color: #8bca67;
+    &:hover {
+      cursor: ew-resize;
+    }
   }
 </style>
 
 <div
+  bind:this={div}
   class="container"
-  style="grid-template-rows: {pos}% 5px auto"
-  bind:clientHeight={size}
-  on:mouseleave={handleMouseup}
+  style="grid-template-{isVertical ? 'columns' : 'rows'}: {pos}% {thickness}px
+  auto; cursor : {dragging ? (isVertical ? 'ew-resize' : 'ns-resize') : ''}"
   on:mousemove={handleMousemove}
   on:mouseup={handleMouseup}>
-  <slot name="first" />
-  <div
-    class={direction === 'vertical' ? 'v-bar' : 'h-bar'}
-    on:mousedown={handleMousedown} />
-  <slot name="second" />
+  <slot name="1" />
+  <div class={isVertical ? 'v-bar' : 'h-bar'} on:mousedown={handleMousedown} />
+  <slot name="2" />
 </div>
